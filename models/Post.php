9fc1fe3e2fc2f55598021f74bff2909f39afd098
Daniel Lwetabe <dannylwe11@gmail.com>
2019-11-08 15:31:36 +0300

    get single post

--- models/Post.php
+++ models/Post.php
@@ -32,6 +32,33 @@
 
             return $stmt;
         }  
+
+        // get single post
+        public function read_single() {
+            $query = 'SELECT c.name as category_name, 
+            p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM ' . $this->table .' p 
+            LEFT JOIN categories c ON p.category_id = c.id
+            WHERE P.id = ?
+            LIMIT 0,1';
+
+            // prepared statement
+            $stmt = $this->conn->prepare($query);
+
+            //bind ID
+            $stmt->bindParam(1, $this->id);
+
+            // execute query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // set properties
+            $this->title = $row['title'];
+            $this->body = $row['body'];
+            $this->author = $row['author'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+        }
     }
 
 ?>
