fc56471320127bcf86c6fbcb119075b88494614f
Daniel Lwetabe <dannylwe11@gmail.com>
2019-11-11 12:26:37 +0300

    update a post

---
+++ api/post/update.php
@@ -0,0 +1,39 @@
+<?php
+// headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+$document_root = $_SERVER['DOCUMENT_ROOT'];
+include($document_root.'/php_rest/config/Databse.php');
+include_once '../../models/Post.php';
+
+// instantiate DB and connect
+$database = new Database();
+$db = $database->connect();
+
+// instantiate blog post
+$post = new Post($db);
+
+// get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// set ID to update
+$post->id = $data->id;
+
+$post->title = $data->title;
+$post->body = $data->body;
+$post->author = $data->author;
+$post->category_id = $data->category_id;
+
+// Update Post
+if($post->update()) {
+    echo json_encode(
+        array('message' => 'Post Updated')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Post Not Updated')
+    );
+}
